b7e8e513c5732556130f0fa70dd722d12993dfed
Neil Stevens <neil@stevensmedia.us>
2015-05-17 13:25:51 -0400

    Pulling the data from WMATA and storing it in an sqlite3 db

--- stats.py
+++ stats.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python
 
 import json
-from pprint import pprint
+import os
+from pprint import pprint, pformat
+import sqlite3
+import sys
 import time
 import urllib
 import urllib2
@@ -26,12 +29,45 @@ def do_wmata_call():
 		return None
 	return result
 
+def createTables(db):
+	tableSQL = """CREATE TABLE data(Time, Car, Destination, DestinationCode, DestinationName, `Group`, Line, LocationCode, LocationName, Min);"""
+	table2SQL = """CREATE TABLE rawdata(time, raw);"""
+	try:
+		db.execute(tableSQL);
+		db.execute(table2SQL);
+		db.commit();
+	except:
+		pass
+
+
 def main():
+	try:
+		filename = sys.argv[1]
+	except:
+		filename = None
+
+	if not filename:
+		print "Database filename required."
+		return None
+
+	try:
+		db = sqlite3.connect(filename)
+	except Exception, e:
+		print "Error opening database: %s" % pformat(e)
+		return None
+	
+	createTables(db)
+
+	insertSQL = """INSERT INTO data(Time, Car, Destination, DestinationCode, DestinationName, `Group`, Line, LocationCode, LocationName, Min) VALUES(DATETIME('now'), :Car, :Destination, :DestinationCode, :DestinationName, :Group, :Line, :LocationCode, :LocationName, :Min);"""
+
 	data = do_wmata_call()
 
 	if not data:
 		return False
 
-	pprint(data)
+	db.execute("INSERT INTO rawdata(time, raw) VALUES(DATETIME('now'), :data);", {"data": pformat(data)})
+	db.commit();
+	db.executemany(insertSQL, data['Trains'])
+	db.commit();
 
 main()
